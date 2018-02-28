804647ab783c97003ca935e99b51156bfed6661a
Matthew Tesch <matt@hebirobotics.com>
2018-02-28 14:08:17 -0500

    Added a basic "teach repeat" example for the arm kits; removed old one.

---
+++ util/trajectory_time_heuristic.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Eigen/Dense"
+
+namespace hebi {
+namespace util {
+
+/**
+ * Get suggested times to reach each waypoint through a simple heuristic.
+ */
+Eigen::VectorXd getTimesForTrajectory(
+  const Eigen::MatrixXd& positions,
+  const Eigen::MatrixXd& velocities,
+  const Eigen::MatrixXd& accelerations)
+{
+  Eigen::VectorXd times(positions.cols());
+  // TODO: add better heuristic, using joint velocity limits for each module and
+  // amount of rotation.
+  for (int i = 0; i < positions.cols(); ++i)
+    times(i) = i * 2;
+  return times;
+}
+
+} // namespace util
+} // namespace hebi
+
