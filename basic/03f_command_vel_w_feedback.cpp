f0e4e669ed60b0894f9e244b3b9dd5b35b4f9bc4
Matthew Tesch <matt@hebirobotics.com>
2018-10-09 13:41:07 -0400

    Added closed loop example and tweaked default module name/family.

---
+++ basic/03f_command_vel_w_feedback.cpp
@@ -0,0 +1,62 @@
+/**
+ * Send velocity commands based on feedback from a module's internal gyro,
+ * and log in the background.
+ *
+ * For more information, go to http://docs.hebi.us/tools.html#cpp-api
+ *
+ * This script assumes you can create a group with 1 module.
+ *
+ * HEBI Robotics
+ * September 2018
+ */
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include "lookup.hpp"
+#include "group_command.hpp"
+#include "group_feedback.hpp"
+
+using namespace hebi;
+
+int main() {
+  // Get group
+  Lookup lookup;
+  auto group = lookup.getGroupFromNames({"Test Family"}, {"Test Actuator" });
+
+  if (!group) {
+    std::cout
+      << "Group not found! Check that the family and name of a module on the network" << std::endl
+      << "matches what is given in the source file." << std::endl;
+    return -1;
+  }
+
+  //// Closed-loop controller (velocity)
+  GroupCommand group_command(group->size());
+  GroupFeedback group_feedback(group->size());
+  
+  // Start logging in the background
+  group->startLog("logs");
+
+  std::cout << "  Move the module to make the output move..." << std::endl;
+
+  double duration = 15;               // [sec]
+  auto start = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> t(std::chrono::system_clock::now() - start);
+  while (t.count() < duration) {
+    group->getNextFeedback(group_feedback);
+    t = std::chrono::system_clock::now() - start;
+
+    // Command a velocity that counters the measured angular velocity around the
+    // z-axis (same axis as the output)
+    auto gyro_z = group_feedback.getGyro().col(2);
+    group_command.setVelocity(-gyro_z);
+    group->sendCommand(group_command);
+  }
+
+  // Stop logging
+  auto log_file = group->stopLog();
+
+  return 0;
+}
