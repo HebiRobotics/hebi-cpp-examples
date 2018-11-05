d64c0d83742085c804339a8134c2d1376ad1110d
Matthew Tesch <matt@hebirobotics.com>
2018-11-05 13:18:25 -0500

    Update daisy start process to properly wait for networking to start.

--- kits/daisy/daisyStart.sh
+++ kits/daisy/daisyStart.sh
@@ -1,4 +1,16 @@
+#!/bin/bash
+
 # Change directory to the current path
 cd $(dirname $(readlink -f $0))/build
-sleep 3 && x-terminal-emulator -e "./hexapod_control -q"
+
+#Wait until networking is active
+while true; do
+  if (systemctl -q is-active network-online.target)
+  then
+    x-terminal-emulator -e "./hexapod_control -q"
+    break
+  fi
+  echo "Waiting for network..."
+  sleep 1
+done
 
