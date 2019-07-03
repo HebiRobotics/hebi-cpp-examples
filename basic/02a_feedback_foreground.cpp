ea6cb8fdfff44eb7f60ae8d199deed7a1b17d0a5
justin-h-m-tunis <justin.tunis44@gmail.com>
2019-07-03 10:40:12 -0400

    Added plotting to 02a_feedback example, updated cmake file to accommodate matplotlibcpp

--- basic/02a_feedback_foreground.cpp
+++ basic/02a_feedback_foreground.cpp
@@ -3,14 +3,17 @@
 #include <thread>
 #include "lookup.hpp"
 #include "group_feedback.hpp"
+#include "matplotlibcpp.h"
 
 using namespace hebi;
+
+namespace plt = matplotlibcpp;
 
 int main()
 {
   // Get a group
   Lookup lookup;
-  auto group = lookup.getGroupFromNames({ "Test Family" }, { "Test Actuator" });
+  auto group = lookup.getGroupFromNames({ "HEBI" }, { "X-00147" });
   if (!group)
   {
     std::cout << "Group not found!" << std::endl;
@@ -25,12 +28,49 @@ int main()
   // constrains the loop to run at the feedback frequency above; we run for
   // about 10 seconds here
   GroupFeedback group_fbk(group->size());
+
+  std::vector<double> y;
+  std::vector<double> x;
+  std::vector<std::string> labels = {"Position","Velocity","Effort"};
+  std::vector<double> label_spacing = {0,1.0,2.0};
+  double y_min = -3.14;
+  double y_max = 3.14;
   for (size_t i = 0; i < 50; ++i)
   { 
     if (group->getNextFeedback(group_fbk))
     {
       std::cout << "Got feedback. Positions are: " << std::endl << group_fbk.getPosition() << std::endl;
+      double position = static_cast<double>(group_fbk.getPosition()[0]);
+      double velocity = static_cast<double>(group_fbk.getVelocity()[0]);
+      double effort = static_cast<double>(group_fbk.getEffort()[0]);
+      y.clear();
+      x.clear();
+      y.push_back(position);
+      y.push_back(velocity);
+      y.push_back(effort);
+      plt::clf();
+      plt::xticks(label_spacing,labels);
+            if ((position > y_max || velocity > y_max || effort > y_max)||(position < y_min || velocity < y_min || effort < y_min)){
+         y_min *= 2;
+	 y_max *= 2;
+      }
+      if ((position < y_max/2 && velocity < y_max/2 && effort < y_max/2) && (position > y_min/2 && velocity > y_min/2 && effort > y_min/2)) {
+        y_max /= 2;
+	y_min /= 2;
+      }
+      std::vector<double> y_ticks;
+      for(double j = 0; j < y_max; j += .5){
+        y_ticks.push_back(j);
+      }
+      for(double j = 0; j > y_min; j -= .5){
+        y_ticks.push_back(j);
+      }
+      plt::yticks(y_ticks);
 
+      plt::grid(true);
+      plt::ylim(y_min, y_max); 
+      plt::bar(y, "black", "-",.5);
+      plt::pause(0.01);
       // Note -- can also retrieve individual module feedback; see API docs.
       // E.g., `group_fbk[0]` is the feedback from the first module.
     }
