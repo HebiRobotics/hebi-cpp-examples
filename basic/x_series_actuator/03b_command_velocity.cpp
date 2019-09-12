8bbba07560d63f5987c53bac3d51cc025a0a224c
HardikSingh97 <singh.hardik1997@gmail.com>
2019-09-12 14:58:25 -0400

    Corrected module names back to generic names

--- basic/x_series_actuator/03b_command_velocity.cpp
+++ basic/x_series_actuator/03b_command_velocity.cpp
@@ -25,7 +25,7 @@ using namespace hebi;
 int main() {
   // Get group
   Lookup lookup;
-  auto group = lookup.getGroupFromNames({"Arm Example"}, {"Wrist1"});
+  auto group = lookup.getGroupFromNames({"Test Family"}, {"Test Actuator"});
 
   if (!group) {
     std::cout
