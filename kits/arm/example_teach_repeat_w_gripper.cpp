9c5bf3f5d1ad854c10a1d507f4e197d6b84f3304
HardikSingh97 <singh.hardik1997@gmail.com>
2019-09-26 19:06:54 -0400

    Adding all relevant files

--- kits/arm/example_teach_repeat_w_gripper.cpp
+++ kits/arm/example_teach_repeat_w_gripper.cpp
@@ -23,6 +23,7 @@ struct Waypoint
   Eigen::VectorXd positions;
   Eigen::VectorXd vels;
   Eigen::VectorXd accels;
+  double grip_effort;
 };
   
 struct State
@@ -31,18 +32,20 @@ struct State
   std::vector<Waypoint> waypoints;
 };
 
-void addWaypoint (State& state, const GroupFeedback& feedback, bool stop) {
+void addWaypoint (State& state, const GroupFeedback& feedback, double grip, bool stop) {
   printf("Adding a Waypoint.\n");
   
   if (stop) { // stop waypoint
     state.waypoints.push_back(Waypoint {feedback.getPosition(),
                               VectorXd::Constant(state.num_modules, 0),
-                              VectorXd::Constant(state.num_modules, 0)});
+                              VectorXd::Constant(state.num_modules, 0),
+                              grip});
   }
   else { // through waypoint
     state.waypoints.push_back(Waypoint {feedback.getPosition(),
                       VectorXd::Constant(state.num_modules, std::numeric_limits<double>::quiet_NaN()),
-                      VectorXd::Constant(state.num_modules, std::numeric_limits<double>::quiet_NaN())});
+                      VectorXd::Constant(state.num_modules, std::numeric_limits<double>::quiet_NaN()),
+                      grip});
   }
 }
 
@@ -54,6 +57,7 @@ arm::Goal playWaypoints (State& state) {
   Eigen::MatrixXd target_vels(state.num_modules, state.waypoints.size());
   Eigen::MatrixXd target_accels(state.num_modules, state.waypoints.size());
   Eigen::VectorXd times(state.waypoints.size());
+  Eigen::MatrixXd aux(1, state.waypoints.size());
 
   // Fill up the relevant matrices
   for (int i = 0; i < state.waypoints.size(); i++)
@@ -62,6 +66,7 @@ arm::Goal playWaypoints (State& state) {
     target_pos.col(i) << state.waypoints[i].positions;
     target_vels.col(i) << state.waypoints[i].vels;
     target_accels.col(i) << state.waypoints[i].accels;
+    aux.col(i) << state.waypoints[i].grip_effort;
   }
 
   // For better motion, we ensure the last waypoint is a stop waypoint
@@ -70,7 +75,7 @@ arm::Goal playWaypoints (State& state) {
   target_accels.col(state.waypoints.size()-1) << 
                     Eigen::VectorXd::Constant(state.num_modules, 0);
 
-  return arm::Goal(target_pos, target_vels, target_accels);
+  return arm::Goal(target_pos, target_vels, target_accels, aux);
 }
 
 double currentTime(std::chrono::steady_clock::time_point& start) {
@@ -93,14 +98,17 @@ int main(int argc, char* argv[])
   // Make sure you are running this from the correct directory!
   params.hrdf_file_ = "kits/hrdf/6-dof_arm.hrdf";  
 
+  // Setup Gripper
+  std::shared_ptr<arm::EffortEndEffector<1>> gripper(arm::EffortEndEffector<1>::create("Arm Example", "Spool").release());
+  params.end_effector_ = gripper;
+
   // Setup Time Variables
   auto start_time = std::chrono::steady_clock::now();
   std::chrono::duration<double> arm_time = std::chrono::steady_clock::now() - start_time;
   double arm_start_time = arm_time.count();
-  
+    
   // Create the Arm Object
   auto arm = arm::Arm::create(arm_start_time, params);
-
 
   /////////////////////////
   //// MobileIO Setup /////
@@ -133,15 +141,29 @@ int main(int argc, char* argv[])
   State state;
   state.num_modules = arm -> robotModel().getDoFCount();
 
+  // Variable to switch gripper control from mobile_io to trajectory and vice versa
+  bool playback = false;
+
+  // Gripper Variables
+
   while(arm->update(currentTime(start_time)))
   {
      // Get latest mobile_state
     auto mobile_state = mobile->getState();
     MobileIODiff diff(last_mobile_state, mobile_state);
 
+    // Testing
+    gripper->getState();
+
+    double open_val = (mobile_state.getAxis(3) * 2)-1; // makes this range between -2 and 1
+    if (!playback) {
+      gripper -> setCommand(0, open_val);
+    }
+    
+
     // Buttton B1 - Add Stop Waypoint
     if (diff.get(1) == MobileIODiff::ButtonState::ToOn) {
-      addWaypoint(state, arm -> lastFeedback(), true);
+      addWaypoint(state, arm -> lastFeedback(), open_val, true);
     }
 
     // Button B2 - Clear Waypoints
@@ -151,7 +173,7 @@ int main(int argc, char* argv[])
 
     // Button B3 - Add Through Waypoint
     if (diff.get(3) == MobileIODiff::ButtonState::ToOn) {
-      addWaypoint(state, arm -> lastFeedback(), false);
+      addWaypoint(state, arm -> lastFeedback(), open_val, false);
     }
 
     // Button B5 - Playback Waypoints
@@ -160,6 +182,7 @@ int main(int argc, char* argv[])
         printf("You have not added any Waypoints!\n");
       } 
       else {
+        playback = true;
         const arm::Goal playback = playWaypoints(state);
         arm -> setGoal(playback);       
       }
@@ -168,6 +191,7 @@ int main(int argc, char* argv[])
     // Button B6 - Grav Comp Mode
     if (diff.get(6) == MobileIODiff::ButtonState::ToOn) {
       // Cancel any goal that is set, returning arm into gravComp mode
+      playback = false;
       arm -> cancelGoal();
     }
 
@@ -190,6 +214,4 @@ int main(int argc, char* argv[])
 
   return 0;
 }
-
-
 
