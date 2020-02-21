9deeb6d1bc6b49cc430070d264923f462a5f8057
HardikSingh97 <singh.hardik1997@gmail.com>
2020-02-20 20:10:43 -0500

    Updated cmake to work with latest CPP

--- projects/cmake/DownloadHebiCpp.cmake
+++ projects/cmake/DownloadHebiCpp.cmake
@@ -1,9 +1,9 @@
 # Used to download the C++ API - this should not be used directly.
 cmake_minimum_required(VERSION 3.0)
 
-set(HEBI_CPP_VERSION "2.1.0")
+set(HEBI_CPP_VERSION "3.2.0")
 set(HEBI_CPP_FILE_NAME "hebi-cpp-${HEBI_CPP_VERSION}.tar.gz")
-set(HEBI_CPP_LIB_SHA256 "91201014ecda682b499c8008fa27446de779fc49ed7abd6827a102c235e7b569")
+set(HEBI_CPP_LIB_SHA256 "db04929188e2443f680d04335166d7c5b274ae12763b18376986426c10545baf")
 set(HEBI_CPP_URL "http://docs.hebi.us/download/cpp/${HEBI_CPP_FILE_NAME}")
 
 # If the CMakeLists.txt is not found, then redownload the C++ API 
