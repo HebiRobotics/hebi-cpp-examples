f5869e7dcc67dba96b040be7a461e2a893d99bab
gvkeerthi <keerthi@hebirobotics.com>
2025-07-21 11:13:28 -0400

    Is chrono time the issue?

--- kits/rosie/rosie_demo.cpp
+++ kits/rosie/rosie_demo.cpp
@@ -302,8 +302,8 @@ int main(int argc, char** argv) {
         ChassisVelocity base_inputs;
         ArmMobileIOInputs arm_inputs;
 
-        bool quit = parse_mobile_feedback(base_inputs, arm_inputs);
-        base_control.update(t, &base_inputs);
+        //bool quit = parse_mobile_feedback(base_inputs, arm_inputs);
+        //base_control.update(t, &base_inputs);
 
         //std::cout << "Hey 3\n";
         //if (arm_control)
