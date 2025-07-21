037308ce519fcf582bbab254fde2fb2b732a1f78
gvkeerthi <keerthi@hebirobotics.com>
2025-07-21 16:41:45 -0400

    Check time

--- kits/rosie/rosie_demo.cpp
+++ kits/rosie/rosie_demo.cpp
@@ -294,6 +294,7 @@ int main(int argc, char** argv) {
     while (base_control.running_ && (!arm_control || arm_control->running())) {
         auto now = std::chrono::system_clock::now();
         std::time_t t = std::chrono::system_clock::to_time_t(now);
+		std::cout << "Current time: " << std::ctime(&t) << std::flush;
 
         bool quit = parse_mobile_feedback(base_inputs, arm_inputs);
         base_control.update(t, &base_inputs);
