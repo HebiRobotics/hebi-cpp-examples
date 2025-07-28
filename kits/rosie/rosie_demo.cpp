a94ad60b7126722ceed048a71b65b79694ef37b1
gvkeerthi <keerthi@hebirobotics.com>
2025-07-28 18:57:51 -0400

    Logs folder for rosie_demo

--- kits/rosie/rosie_demo.cpp
+++ kits/rosie/rosie_demo.cpp
@@ -303,7 +303,6 @@ int main(int argc, char** argv) {
 
     bool enable_logging = true;
     if (enable_logging) {
-        system("mkdir logs");
         base.group_->startLog("./logs");
         arm->group().startLog("./logs");
     }
